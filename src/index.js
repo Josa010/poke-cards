fd10d7f9d0a0e75c48078c477ee4bcd0697a2412
Josa <novairesh@gmail.com>
2021-04-05 11:35:41 -0500

    index js fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import '@styles/main.css';
 import '@styles/Pokemon.css'
 import '@styles/404.css';
 
-import router from './routes';
+import router from './routes/Index.js';
 
 window.addEventListener('load', router)
 window.addEventListener('hashchange', router)
